d3a7f46218104a1f2b0b63bd0ce4cfce44f2e237
jamesread <contact@jread.com>
2024-09-03 07:12:36 -0700

    Remove old postal address

--- contact.php
+++ contact.php
@@ -7,18 +7,6 @@ require_once 'includes/widgets/header.php';
 <h2>Email</h2>
 <p><a href = "">wedding@jamesandevie.com</a></p>
 
-<h2>Postal Address</h2>
-<p>
-Our address is;
-</p>
-
-<blockquote>
-1 Thornfield Green
-Blackwater
-Hampshire
-GU17 9EY
-</blockquote>
-
 <?php
 
 require_once 'includes/widgets/footer.php';
